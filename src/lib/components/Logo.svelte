32d9490aa1ac086686f9146e7d5d267463c64bf4
Florian <dev@fwcr.de>
2026-06-09 01:43:23 +0200

    feat: OpenMapX Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  const mark = '/img/mark.png';
+
+  type Props = {
+    variant?: 'inline' | 'stacked' | 'mark';
+    class?: string;
+  };
+  let { variant = 'inline', class: className = '' }: Props = $props();
+</script>
+
+{#if variant === 'mark'}
+  <img src={mark} alt="OpenMapX" class={className} />
+{:else if variant === 'stacked'}
+  <span class="flex flex-col items-center gap-2 {className}">
+    <img src={mark} alt="" class="size-16" />
+    <span class="text-2xl font-extrabold tracking-tight">OpenMapX</span>
+  </span>
+{:else}
+  <span class="flex items-center gap-2 {className}">
+    <img src={mark} alt="" class="size-8" />
+    <span class="text-xl font-extrabold tracking-tight">OpenMapX</span>
+  </span>
+{/if}
