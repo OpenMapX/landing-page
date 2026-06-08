0fb1b29d2950f407fd9a83b2bb873486cf1ed2fd
Florian <dev@fwcr.de>
2026-06-09 01:33:14 +0200

    chore: standalone svelte/vite/ts config

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import { svelteMarkdownPreprocess } from '@immich/svelte-markdown-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const staticFiles = new Set(['/favicon.ico', '/img/social-preview.png']);
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: ['.svelte', '.md'],
+  preprocess: [
+    svelteMarkdownPreprocess({
+      layouts: {
+        default: '$lib/components/BlogPage.svelte',
+        PrivacyPolicyPage: '$lib/components/PrivacyPolicyPage.svelte',
+      },
+    }),
+    vitePreprocess(),
+  ],
+  kit: {
+    adapter: adapter(),
+    alias: { $common: 'src/common', '$common/*': 'src/common/*' },
+    prerender: {
+      handleHttpError: ({ path, message }) => {
+        if (staticFiles.has(path)) return;
+        throw new Error(message);
+      },
+    },
+  },
+};
+
+export default config;
