6ed6255cf69a4e41a7b27b71f69d43719317834c
Florian <dev@fwcr.de>
2026-06-09 01:43:35 +0200

    feat: OpenMapX site constants module

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const Sites = {
+  App: 'https://openmapx.com',
+  Landing: 'https://openmapx.org',
+  Docs: 'https://openmapx.com/docs',
+};
+
+export const Socials = {
+  Github: 'https://github.com/OpenMapX',
+};
+
+export const SelfHost = {
+  Repo: 'https://github.com/OpenMapX',
+  Compose: 'https://github.com/OpenMapX/openmapx#self-hosting',
+};
